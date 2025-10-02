5edb8006d83730f7b07d1f635b32d9566d897b45
Beto <vargasmorenoalberto@gmail.com>
2025-10-01 20:30:53 -0600

    refactor: extract chart to function
    - extract chart initialization to a function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,33 @@ import fetchData from "../utils/fetchData";
 import HeroTemplate from "../templates/HeroTemplate";
 import Chart from 'chart.js/auto';
 
+const initializeChart = async () => {
+    const data = [
+        { year: 2010, count: 10 },
+        { year: 2011, count: 20 },
+        { year: 2012, count: 15 },
+        { year: 2013, count: 25 },
+        { year: 2014, count: 22 },
+        { year: 2015, count: 30 },
+        { year: 2016, count: 28 },
+    ];
+
+    const ctx = document.getElementById('acquisitions');
+    const options = {
+        type: 'bar',
+        data: {
+            labels: data.map(row => row.year),
+            datasets: [
+                {
+                    label: 'Acquisitions by year',
+                    data: data.map(row => row.count)
+                }
+            ]
+        }
+    };
+    new Chart(ctx, options);
+}
+
 const Home = async () => {
     const characters = await fetchData();
     const charactersArray = characters?.results.map(character => character);
@@ -26,44 +53,17 @@ const Home = async () => {
     const view = `
     <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     <h1>This the home page!</h1>
-    ${heroSection}
-    
     <div class="chart-container" style="width: 800px; margin: 20px auto;">
         <canvas id="acquisitions"></canvas>
     </div>
-    
+    ${heroSection}
     <div class="Characters">
         ${charactersCards}
     </div>
     `;
 
-    // Initialize chart after the DOM is updated
     setTimeout(() => {
-        const data = [
-            { year: 2010, count: 10 },
-            { year: 2011, count: 20 },
-            { year: 2012, count: 15 },
-            { year: 2013, count: 25 },
-            { year: 2014, count: 22 },
-            { year: 2015, count: 30 },
-            { year: 2016, count: 28 },
-        ];
-
-        new Chart(
-            document.getElementById('acquisitions'),
-            {
-                type: 'bar',
-                data: {
-                    labels: data.map(row => row.year),
-                    datasets: [
-                        {
-                            label: 'Acquisitions by year',
-                            data: data.map(row => row.count)
-                        }
-                    ]
-                }
-            }
-        );
+        initializeChart();
     }, 0);
 
     return view;
