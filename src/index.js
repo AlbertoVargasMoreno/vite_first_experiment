4554bd89db65072d3e4c44c387f8870637c16cf0
Beto <vargasmorenoalberto@gmail.com>
2025-10-01 19:21:43 -0600

    refactor: reorder directories, all contained in src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import router from '../routes/';
-import '../css/style.css'
-import '../css/gallery.css'
+import router from './routes/';
+import './css/style.css'
+import './css/gallery.css'
 
 window.addEventListener('load', router);
 window.addEventListener('hashchange', router);
